a1e24df116785e9390db36a653a2b43126beafd0
prabhat1001 <prabhatsaraswat24@gmail.com>
2024-06-21 13:02:42 +0530

    feat: created a Navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css'
+
+const NavBar = () => {
+  return (
+    <div className='nav-cont'>
+        <h1 className='logo-txt'>CapGen.</h1>
+        <a className='gtb-btn' href='https://github.com/prabhat1001/CapGen'>GitHub</a>
+    </div>
+  )
+}
+
+export default NavBar
